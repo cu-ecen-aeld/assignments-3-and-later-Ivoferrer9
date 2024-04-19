eb866982a0f01d9171f2a1eaf4eb928135bbd672
Ivoferrer9 <ivoferrer9@gmail.com>
2024-04-19 19:21:58 +0800

    update2

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Check if the number of arguments are equal to 2
+
 if [ "$#" -ne 2 ]; then
     echo "Error: Two arguments are required."
     exit 1
 fi
-#assign first argument as writefile
+
 writefile="$1"
 
-#assign second argument as content
 writestr="$2"
 if [ -z "$writefile" ] || [ -z "$writestr" ]; then
     echo "Error: Arguments cannot be empty."
@@ -16,10 +15,10 @@ if [ -z "$writefile" ] || [ -z "$writestr" ]; then
 fi
 mkdir -p "$(dirname "$writefile")"
 
-# Write the content to the file
+
 echo "$writestr" > "$writefile"
 
-# the returned value is checked if there is error
+
 if [ "$?" -ne 0 ]; then
     echo "Error: Failed to write to the file."
     exit 1
